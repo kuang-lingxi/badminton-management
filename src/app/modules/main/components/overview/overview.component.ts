1c8a6de34dda8136370f1417bbbff0b138400a2b
kuanglingxi <1148273143@qq.com>
2020-02-04 17:51:31 +0800

    ADD: page overview, ui to be perfect

--- src/app/modules/main/components/overview/overview.component.ts
+++ src/app/modules/main/components/overview/overview.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
+  topMatch: any = [
+    {id: 1, img: '', title: '3v3趣味赛', introduce: '这是一条描述信息这是一条描述信息这是一条描述信息', author: 'klx', time: 1580808088000},
+    {id: 2, img: '', title: '3v3趣味赛', introduce: '这是一条描述信息这是一条描述信息这是一条描述信息', author: 'klx', time: 1580808088000},
+    {id: 3, img: '', title: '3v3趣味赛', introduce: '这是一条描述信息这是一条描述信息这是一条描述信息', author: 'klx', time: 1580808088000},
+    {id: 4, img: '', title: '3v3趣味赛', introduce: '这是一条描述信息这是一条描述信息这是一条描述信息', author: 'klx', time: 1580808088000},
+    {id: 5, img: '', title: '3v3趣味赛', introduce: '这是一条描述信息这是一条描述信息这是一条描述信息', author: 'klx', time: 1580808088000},
+    {id: 6, img: '', title: '3v3趣味赛', introduce: '这是一条描述信息这是一条描述信息这是一条描述信息', author: 'klx', time: 1580808088000}
+  ];
+
+  dynamicList: any = [
+    {id: 1, content: '这是动态'},
+    {id: 1, content: '这是动态'},
+    {id: 1, content: '这是动态'},
+    {id: 1, content: '这是动态'},
+    {id: 1, content: '这是动态'},
+    {id: 1, content: '这是动态'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
