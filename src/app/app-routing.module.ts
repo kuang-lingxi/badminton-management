18627a6239676913b96d82561ef404e2068d42e3
kuanglingxi <1148273143@qq.com>
2020-02-04 10:46:30 +0800

    ADD: add page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
