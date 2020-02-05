34d628174b465d6a145882619c51a824c8d70a67
kuanglingxi <1148273143@qq.com>
2020-02-05 17:00:27 +0800

    ADD: add page match-list

--- src/app/modules/main/match/components/match-list/match-list.component.ts
+++ src/app/modules/main/match/components/match-list/match-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { matchStatusEnum } from '../../data/match-status.enum';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-match-list',
@@ -6,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./match-list.component.scss']
 })
 export class MatchListComponent implements OnInit {
+  validateForm: FormGroup;
 
-  constructor() { }
+  pageSize: number = 10;
+
+  pageIndex: number = 1;
+
+  total: number = 30;
+
+  matchStatus: any = matchStatusEnum;
+
+  matchData: any = [
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 0, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 1, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 2, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 3, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 4, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 5, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 0, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 0, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 0, endTime: 1580808088000},
+    {id: 1, name: '3v3趣味赛', introduce: '这是一段简介', people: 20, status: 0, endTime: 1580808088000}
+  ]
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    
+  }
 
   ngOnInit() {
+    this.validateForm = this.fb.group({
+      matchName: [null, [Validators.required]],
+      status: [this.matchStatus.enrolling, [Validators.required]],
+    });
+  }
+
+  submitForm(): void {
+    const data = {
+      ...this.validateForm.value
+    }
+
+    console.log(data);
+  }
+
+  formatMatchStatus(status) {
+    switch(status) {
+      case this.matchStatus.waitBegin: return "待开启";
+      case this.matchStatus.enrolling: return "报名中";
+      case this.matchStatus.enrollEnd: return "报名截止";
+      case this.matchStatus.matching: return "比赛中";
+      case this.matchStatus.numMax: return "人数已满";
+      default: return "未知";
+    }
   }
 
 }
---
+++ src/app/modules/main/match/data/match-status.enum.ts
@@ -0,0 +1,7 @@
+export enum matchStatusEnum {
+  waitBegin = 0,
+  enrolling = 1,
+  enrollEnd = 2,
+  matching = 3,
+  numMax = 4
+}
