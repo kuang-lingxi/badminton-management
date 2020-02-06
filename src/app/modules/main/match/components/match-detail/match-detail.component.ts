3d75d89a44fca84f5bb0d704576853560a06f357
kuanglingxi <1148273143@qq.com>
2020-02-06 11:09:27 +0800

    ADD: add page match-detail

--- src/app/modules/main/match/components/match-detail/match-detail.component.ts
+++ src/app/modules/main/match/components/match-detail/match-detail.component.ts
@@ -7,6 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchDetailComponent implements OnInit {
 
+  peopleOption = {
+      tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+          orient: 'vertical',
+          left: 10,
+          data: ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D']
+      },
+      series: [
+          {
+              name: '人员等级',
+              type: 'pie',
+              radius: ['50%', '70%'],
+              avoidLabelOverlap: false,
+              label: {
+                  normal: {
+                      show: false,
+                      position: 'center'
+                  },
+                  emphasis: {
+                      show: true,
+                      textStyle: {
+                          fontSize: '30',
+                          fontWeight: 'bold'
+                      }
+                  }
+              },
+              labelLine: {
+                  normal: {
+                      show: false
+                  }
+              },
+              data: [
+                  {value: 1, name: 'A+'},
+                  {value: 10, name: 'A'},
+                  {value: 30, name: 'B+'},
+                  {value: 20, name: 'B'},
+                  {value: 10, name: 'C+'},
+                  {value: 2, name: 'C'},
+                  {value: 1, name: 'D'}
+              ]
+          }
+      ]
+  };
+
   constructor() { }
 
   ngOnInit() {
