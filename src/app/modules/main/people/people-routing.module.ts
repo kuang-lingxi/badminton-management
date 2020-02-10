efd90ba9ce527905499e288026e8db3207b07d49
kuanglingxi <1148273143@qq.com>
2020-02-10 16:59:30 +0800

    ADD: add page people-detail

--- src/app/modules/main/people/people-routing.module.ts
+++ src/app/modules/main/people/people-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PeopleDetailComponent } from './components/people-detail/people-detail.component';
 
 
 const routes: Routes = [
   {
     path: 'list',
     component: PeopleListComponent
+  },
+  {
+    path: 'list/:id',
+    component: PeopleDetailComponent
   }
 ];
 
--- src/app/modules/main/people/people.module.ts
+++ src/app/modules/main/people/people.module.ts
@@ -5,10 +5,11 @@ import { SelfCommonModule } from '../../common/common.module';
 import { PeopleRoutingModule } from './people-routing.module';
 import { PeopleListComponent } from './components/people-list/people-list.component';
 import { PeopleItemComponent } from './components/people-item/people-item.component';
+import { PeopleDetailComponent } from './components/people-detail/people-detail.component';
 
 
 @NgModule({
-  declarations: [PeopleListComponent, PeopleItemComponent],
+  declarations: [PeopleListComponent, PeopleItemComponent, PeopleDetailComponent],
   imports: [
     CommonModule,
     PeopleRoutingModule,
