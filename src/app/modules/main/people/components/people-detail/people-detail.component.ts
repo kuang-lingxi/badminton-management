175f33a8bec0704f54bc4ca04f9e10359437ea22
kuanglingxi <1148273143@qq.com>
2020-02-10 17:45:57 +0800

    MOD: improve people-detail page

--- src/app/modules/main/people/components/people-detail/people-detail.component.ts
+++ src/app/modules/main/people/components/people-detail/people-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-people-detail',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  userInfo: any;
+
+  oldUserInfo: any;
+
+  validateForm: FormGroup;
+
+  forbidEdit: boolean = true;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.userInfo = {
+      name: 'klx',
+      introduce: 'introduce',
+      phone: '17801170810',
+      member: 1,
+      referee: 1,
+      grade: 100,
+      admin: 1
+    }
+    this.oldUserInfo = {
+      ...this.userInfo
+    }
+    this.validateForm = this.fb.group({
+      name: [this.userInfo.name, [Validators.required]],
+      phone: [this.userInfo.phone],
+      introduce: [this.userInfo.introduce],
+      member:[this.userInfo.member],
+      referee: [this.userInfo.referee],
+      grade: [this.userInfo.grade],
+      admin: [this.userInfo.admin]
+    });
+  }
+
+  edit() {
+    this.forbidEdit = false;
+  }
+
+  submitForm() {
+    console.log(this.validateForm.value)
+  }
+
+  reset() {
+    this.validateForm = this.fb.group({
+      name: [this.userInfo.name, [Validators.required]],
+      phone: [this.userInfo.phone],
+      introduce: [this.userInfo.introduce],
+      member:[this.userInfo.member],
+      referee: [this.userInfo.referee],
+      grade: [this.userInfo.grade],
+      admin: [this.userInfo.admin]
+    });
+    this.forbidEdit = true;
   }
 
 }
