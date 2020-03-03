6b1cf4cb661bb1fb1775b609abc46d775f7e1704
kuanglingxi <1148273143@qq.com>
2020-03-03 11:03:29 +0800

    ADD: add login redirect

--- src/app/modules/login/components/login/login.component.ts
+++ src/app/modules/login/components/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from '../../service/login.service';
-import { Response } from '../../../common/data/response'; 
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private nzMessageService: NzMessageService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -38,6 +41,9 @@ export class LoginComponent implements OnInit {
       if(response.code === 0) {
         if(response.message.result) {
           this.loginService.setToken(response.message.token, userMsg.remember);
+          this.router.navigateByUrl("/main/overview");w
+        }else {
+          this.nzMessageService.create('error', '用户名或密码错误');
         }
       }
     })
